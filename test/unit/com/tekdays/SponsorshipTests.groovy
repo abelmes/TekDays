7db23d57d055150cb7501833b9692992201e4ef8
abel <abelmesfin88@gmail.com>
2016-03-18 14:45:00 +0300

    upload

---
+++ test/unit/com/tekdays/SponsorshipTests.groovy
@@ -0,0 +1,16 @@
+package com.tekdays
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Sponsorship)
+class SponsorshipTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
